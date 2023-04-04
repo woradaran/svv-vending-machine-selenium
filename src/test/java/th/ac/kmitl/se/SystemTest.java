8315b792071d5989a0c0feeeaeeab2a99da5115e
nj.kmitl <natthapong.ju@kmitl.ac.th>
2023-04-04 16:30:57 +0700

    Initial

---
+++ src/test/java/th/ac/kmitl/se/SystemTest.java
@@ -0,0 +1,15 @@
+package th.ac.kmitl.se;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SystemTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void test1() throws java.io.IOException {
+        System.out.println("Test 1");
+    }
+}
