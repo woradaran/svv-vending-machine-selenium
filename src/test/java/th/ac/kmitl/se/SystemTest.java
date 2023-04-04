0ca9fa83bdd987e39a3f7ebead992c8905070b8e
nj.kmitl <natthapong.ju@kmitl.ac.th>
2023-04-04 16:43:53 +0700

    Initial

--- src/test/java/th/ac/kmitl/se/SystemTest.java
+++ src/test/java/th/ac/kmitl/se/SystemTest.java
@@ -2,6 +2,14 @@ package th.ac.kmitl.se;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SystemTest {
     @BeforeEach
