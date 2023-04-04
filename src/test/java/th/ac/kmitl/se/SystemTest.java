00e7aad07aee3036e4d81ab4df1aa5026515d5eb
nj.kmitl <natthapong.ju@kmitl.ac.th>
2023-04-04 17:23:26 +0700

    Initial

--- src/test/java/th/ac/kmitl/se/SystemTest.java
+++ src/test/java/th/ac/kmitl/se/SystemTest.java
@@ -1,5 +1,8 @@
 package th.ac.kmitl.se;
 
+import org.graphwalker.java.annotation.AfterExecution;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,12 +15,22 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SystemTest {
-    @BeforeEach
+    WebDriver driver;
+    @BeforeAll
     void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.get("https://fekmitl.pythonanywhere.com/kratai-bin");
+    }
+    @AfterAll
+    public void tearDown() {
+        driver.quit();
     }
 
     @Test
-    public void test1() throws java.io.IOException {
+    public void test1() {
         System.out.println("Test 1");
     }
 }
