d0c4f2500d6094539f1873b7d667a5eca74ffc2a
woradaran <57227460+woradaran@users.noreply.github.com>
2023-04-17 23:04:27 +0700

    fix

--- src/test/java/th/ac/kmitl/se/SystemTest.java
+++ src/test/java/th/ac/kmitl/se/SystemTest.java
@@ -1,10 +1,9 @@
 package th.ac.kmitl.se;
 
+import com.ibm.icu.impl.Assert;
 import org.graphwalker.java.annotation.AfterExecution;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.openqa.selenium.*;
@@ -12,27 +11,208 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
+import java.time.Duration;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SystemTest {
-    static WebDriver driver;
-    
-    @BeforeAll
-    static public void setUp() {
+    WebDriver driver;
+    @BeforeEach
+    void setUp() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
         driver.get("https://fekmitl.pythonanywhere.com/kratai-bin");
     }
-
     @AfterAll
-    static public void tearDown() {
+    public void tearDown() {
         driver.quit();
     }
 
+
+    //Test Collection Error
     @Test
-    public void test1() {
-        System.out.println("Test 1");
+    public void test1() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+
+        
+        WebElement starButton = driver.findElement(By.id("start"));
+
+   
+        starButton.click();
+
+        wait.until(ExpectedConditions.urlToBe("https://fekmitl.pythonanywhere.com/kratai-bin/order"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("add_tum_thai")));
+
+        WebElement tumThaiOrder = driver.findElement(By.id("add_tum_thai"));
+        WebElement tumPooOrder = driver.findElement(By.id("add_tum_poo"));
+        WebElement btnCheckOut = driver.findElement(By.id("btn_check_out"));
+
+
+        tumThaiOrder.click();
+        tumPooOrder.click();
+
+        Thread.sleep(1000); 
+
+        btnCheckOut.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("btn_confirm")));
+        WebElement btnConfirm = driver.findElement(By.id("btn_confirm"));
+
+        btnConfirm.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("btn_pay")));
+
+        WebElement textOnCreditCard = driver.findElement(By.name("txt_credit_card_num"));
+        textOnCreditCard.sendKeys("1010101");
+
+        WebElement nameOnCardInput = driver.findElement(By.name("txt_name_on_card"));
+        nameOnCardInput.sendKeys("CREAM");
+
+        WebElement btnPay = driver.findElement(By.name("btn_pay"));
+        btnPay.click();
+
+        WebElement imageTumThaiElement = driver.findElement(By.className("ImgTumThai"));
+        WebElement imageTumPooElement = driver.findElement(By.className("ImgTumPoo"));
+
+
+        Thread.sleep(1000*15); 
+
+        driver.quit();
+    }
+
+    //Test payment error
+    @Test
+    public void test2() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+
+        WebElement starButton = driver.findElement(By.id("start"));
+        starButton.click();
+
+        wait.until(ExpectedConditions.urlToBe("https://fekmitl.pythonanywhere.com/kratai-bin/order"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("add_tum_thai")));
+
+
+        WebElement tumThaiOrder = driver.findElement(By.id("add_tum_thai"));
+        WebElement tumPooOrder = driver.findElement(By.id("add_tum_poo"));
+        WebElement btnCheckOut = driver.findElement(By.id("btn_check_out"));
+
+
+        tumThaiOrder.click();
+        tumPooOrder.click();
+
+        Thread.sleep(1000); 
+
+        btnCheckOut.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("btn_confirm")));
+        WebElement btnConfirm = driver.findElement(By.id("btn_confirm"));
+
+        btnConfirm.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("btn_pay")));
+
+
+        WebElement btnPay = driver.findElement(By.name("btn_pay"));
+        btnPay.click();
+        Thread.sleep(1000); 
+        WebElement btnPay2 = driver.findElement(By.name("btn_pay"));
+        btnPay2.click();
+        Thread.sleep(1000); 
+        WebElement btnPay3 = driver.findElement(By.name("btn_pay"));
+        btnPay3.click();
+        Thread.sleep(2000); 
+
+        driver.quit();
+    }
+    //Success test
+    @Test
+    public void test3() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+
+        WebElement startButton = driver.findElement(By.id("start"));
+
+        startButton.click();
+        // welcome
+        wait.until(ExpectedConditions.urlToBe("https://fekmitl.pythonanywhere.com/kratai-bin/order"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("add_tum_thai")));
+
+        WebElement cancelButton = driver.findElement(By.id("btn_cancel"));
+        cancelButton.click();
+
+        Thread.sleep(1000);
+
+        WebElement startButton2 = driver.findElement(By.id("start"));
+        startButton2.click();
+
+        wait.until(ExpectedConditions.urlToBe("https://fekmitl.pythonanywhere.com/kratai-bin/order"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("add_tum_thai")));
+
+        WebElement tumThaiOrder = driver.findElement(By.id("add_tum_thai"));
+        WebElement tumPooOrder = driver.findElement(By.id("add_tum_poo"));
+        WebElement btnCheckOut = driver.findElement(By.id("btn_check_out"));
+
+
+        tumThaiOrder.click();
+        tumThaiOrder.click();
+        tumThaiOrder.click();
+        tumThaiOrder.click();
+
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        Thread.sleep(1000); 
+        tumPooOrder.click();
+
+        Thread.sleep(1000); 
+
+        btnCheckOut.click();
+        Thread.sleep(1000);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("btn_confirm")));
+
+
+        WebElement btnChange = driver.findElement(By.id("btn_change"));
+        btnChange.click();
+
+        WebElement tumPooOrder2 = driver.findElement(By.id("add_tum_poo"));
+        WebElement btnCheckOut2 = driver.findElement(By.id("btn_check_out"));
+
+        tumPooOrder2.click();
+        tumPooOrder2.click();
+
+        Thread.sleep(1000);
+        btnCheckOut2.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("btn_confirm")));
+        WebElement btnConfirm = driver.findElement(By.id("btn_confirm"));
+        btnConfirm.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("btn_pay")));
+
+        WebElement btnPay = driver.findElement(By.name("btn_pay"));
+        btnPay.click();
+
+        WebElement textOnCreditCard = driver.findElement(By.name("txt_credit_card_num"));
+        textOnCreditCard.sendKeys("1010101");
+
+        WebElement nameOnCardInput = driver.findElement(By.name("txt_name_on_card"));
+        nameOnCardInput.sendKeys("CREAM");
+
+        WebElement btnPay2 = driver.findElement(By.name("btn_pay"));
+        btnPay2.click();
+
+        WebElement imageTumThaiElement = driver.findElement(By.className("ImgTumThai"));
+        WebElement imageTumPooElement = driver.findElement(By.className("ImgTumPoo"));
+
+        imageTumPooElement.click();
+        imageTumThaiElement.click();
+
+        Thread.sleep(1000);
+
+
+        driver.quit();
     }
 }
--- src/test/java/th/ac/kmitl/se/VendingMachineAdapter.java
+++ src/test/java/th/ac/kmitl/se/VendingMachineAdapter.java
@@ -19,8 +19,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 @Model(file  = "VendingMachineV2.json")
 public class VendingMachineAdapter extends ExecutionContext {
     WebDriver driver;
+    WebDriverWait w;
     static final float PRICE_TUM_THAI = 100.0f;
     static final float PRICE_TUM_POO = 120.0f;
+    static int numTumThai = 0;
+    static int numTumPoo = 0;
+    static int retryPayCount = 0;
 
     @BeforeExecution
     public void setUp() {
@@ -29,6 +33,7 @@ public class VendingMachineAdapter extends ExecutionContext {
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
         driver.get("https://fekmitl.pythonanywhere.com/kratai-bin");
+        w = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @AfterExecution
@@ -41,6 +46,9 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Vertex WELCOME");
         new WebDriverWait(driver, Duration.ofSeconds(5))
                 .until(ExpectedConditions.elementToBeClickable(By.id("start")));
+        numTumThai = 0;
+        numTumPoo = 0;
+        retryPayCount = 0;
     }
 
     @Edge()
@@ -54,11 +62,13 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Vertex ORDERING");
         // Wait for the check-out button to be clickable.
         // Your code here ...
-
+        // w.until(ExpectedConditions.elementToBeClickable(By.id("btn_check_out")));
         // Check that the number of orders is as expected.
         int numTumThaiExpected = getAttribute("numTumThai").asInt();
         int numTumPooExpected = getAttribute("numTumPoo").asInt();
         // Your code here ...
+        assertEquals(numTumThai, numTumThaiExpected);
+        assertEquals(numTumPoo, numTumPooExpected);
 
     }
 
@@ -67,6 +77,9 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Edge addTumThai");
         // Click add tum thai
         // Your code here ...
+        w.until(ExpectedConditions.elementToBeClickable(By.id("add_tum_thai")));
+        driver.findElement(By.id("add_tum_thai")).click();
+        numTumThai++;
     }
 
     @Edge()
@@ -74,6 +87,9 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Edge addTumPoo");
         // Click add tum poo
         // Your code here ...
+        w.until(ExpectedConditions.elementToBeClickable(By.id("add_tum_poo")));
+        driver.findElement(By.id("add_tum_poo")).click();
+        numTumPoo++;
     }
 
     @Vertex()
@@ -81,6 +97,14 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Vertex ERROR_ORDERING");
         // Wait for the alert dialog to be visible.
         // Your code here ...
+        // maybe decrease number to 3
+        if (getLastElement().getName().equals("addTumPoo")) {
+            numTumPoo--;
+        } else if (getLastElement().getName().equals("addTumThai")) {
+            numTumThai--;
+        }
+
+        w.until(ExpectedConditions.alertIsPresent());
     }
 
     @Edge()
@@ -88,6 +112,7 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Edge ack");
         // Click OK on the alert dialog
         // Your code here ...
+        driver.switchTo().alert().accept();
     }
 
     @Edge()
@@ -95,6 +120,8 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Edge cancel");
         // Click cancel button
         // Your code here ...
+        w.until(ExpectedConditions.elementToBeClickable(By.id("btn_cancel")));
+        driver.findElement(By.id("btn_cancel")).click();
     }
 
     @Edge()
@@ -102,6 +129,10 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Edge checkOut");
         // Click check out button and wait for the confirm button to be clickable.
         // Your code here ...
+        w.until(ExpectedConditions.elementToBeClickable(By.id("btn_check_out")));
+        driver.findElement(By.id("btn_check_out")).click();
+
+        w.until(ExpectedConditions.elementToBeClickable(By.id("btn_confirm")));
     }
 
     @Vertex()
@@ -109,11 +140,13 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Vertex CONFIRMING");
         // Wait for the confirm button to be clickable
         // Your code here ...
-
+        w.until(ExpectedConditions.elementToBeClickable(By.id("btn_confirm")));
         // Check that the information shown is as expected.
         int numTumThaiExpected = getAttribute("numTumThai").asInt();
         int numTumPooExpected = getAttribute("numTumPoo").asInt();
         // Your code here ...
+        assertEquals(numTumThai, numTumThaiExpected);
+        assertEquals(numTumPoo, numTumPooExpected);
     }
 
     @Edge()
@@ -121,7 +154,8 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Edge change");
         // Click change button
         // Your code here ...
-
+        w.until(ExpectedConditions.elementToBeClickable(By.id("btn_change")));
+        driver.findElement(By.id("btn_change")).click();
     }
 
     @Edge()
@@ -129,6 +163,7 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Edge pay");
         // Click Confirm button
         // Your code here ...
+        driver.findElement(By.id("btn_confirm")).click();
 
     }
 
@@ -137,15 +172,22 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Vertex PAYING");
         // Wait for the pay button to be clickable.
         // Your code here ...
-
+        w.until(ExpectedConditions.elementToBeClickable(By.id("btn_pay")));
+        // error_pay
         // Check that the total amount is as expected.
         int numTumThaiExpected = getAttribute("numTumThai").asInt();
         int numTumPooExpected = getAttribute("numTumPoo").asInt();
         // Your code here ...
+        assertEquals(numTumThai, numTumThaiExpected);
+        assertEquals(numTumPoo, numTumPooExpected);
 
         // Check that payment error message is properly shown
-        // Hint: Use getLastElement().getName() to get the name of the last visited edge.
+        // Hint: Use getLastElement().getName() to get the name of the last visited
+        // edge.
         // Your code here ...
+        if (getLastElement().getName().equals("payRetry")) {
+            w.until(ExpectedConditions.visibilityOfElementLocated(By.id("msg_error")));
+        }
 
     }
 
@@ -154,6 +196,12 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Edge paid");
         // Submit valid payment details
         // Your code here ...
+        WebElement text_input = driver.findElement(By.name("txt_credit_card_num"));
+        text_input.sendKeys("1234567890123456");
+        text_input = driver.findElement(By.name("txt_name_on_card"));
+        text_input.sendKeys("Adam Smith");
+        w.until(ExpectedConditions.elementToBeClickable(By.id("btn_pay")));
+        driver.findElement(By.id("btn_pay")).click();
 
     }
 
@@ -162,6 +210,12 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Edge payError");
         // Submit blank payment details to simulate payment error
         // Your code here ...
+        WebElement text_input = driver.findElement(By.name("txt_credit_card_num"));
+        text_input.sendKeys("");
+        text_input = driver.findElement(By.name("txt_name_on_card"));
+        text_input.sendKeys("");
+        w.until(ExpectedConditions.elementToBeClickable(By.id("btn_pay")));
+        driver.findElement(By.id("btn_pay")).click();
     }
 
     @Vertex()
@@ -172,6 +226,8 @@ public class VendingMachineAdapter extends ExecutionContext {
     @Edge()
     public void payRetry() {
         System.out.println("Edge payRetry");
+        retryPayCount++;
+
     }
 
     @Vertex()
@@ -179,11 +235,15 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Vertex COLLECTING");
         // Wait for images to be clickable
         // Your code here ...
-
+        w.until(ExpectedConditions.elementToBeClickable(By.tagName("img")));
         // Check that the number of items shown is correct
         int numTumThaiExpected = getAttribute("numTumThai").asInt();
         int numTumPooExpected = getAttribute("numTumPoo").asInt();
         // Your code here ...
+        List<WebElement> imgs_thai = driver.findElements(By.ByClassName.className("ImgTumThai"));
+        List<WebElement> imgs_poo = driver.findElements(By.ByClassName.className("ImgTumPoo"));
+        assertEquals(numTumPooExpected, imgs_poo.size());
+        assertEquals(numTumThaiExpected, imgs_thai.size());
 
     }
 
@@ -192,6 +252,14 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Edge collected");
         // Click on each image to collect all dishes
         // Your code here ...
+        for (int i = 0; i < numTumPoo; i++) {
+            w.until(ExpectedConditions.elementToBeClickable(By.ByClassName.className("ImgTumPoo")));
+            driver.findElement(By.ByClassName.className("ImgTumPoo")).click();
+        }
+        for (int i = 0; i < numTumThai; i++) {
+            w.until(ExpectedConditions.elementToBeClickable(By.ByClassName.className("ImgTumThai")));
+            driver.findElement(By.ByClassName.className("ImgTumThai")).click();
+        }
 
     }
 
@@ -200,6 +268,9 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Edge collectError");
         // Wait until the clearing page is shown
         // Your code here ...
+
+        WebDriverWait spw = new WebDriverWait(driver, Duration.ofSeconds(15));
+        spw.until(ExpectedConditions.visibilityOfElementLocated(By.id("msg_clearing")));
 
     }
 
@@ -213,6 +284,6 @@ public class VendingMachineAdapter extends ExecutionContext {
         System.out.println("Edge cleared");
         // Wait until redirection to the welcome page
         // Your code here ...
-
+        w.until(ExpectedConditions.elementToBeClickable(By.id("start")));
     }
 }
